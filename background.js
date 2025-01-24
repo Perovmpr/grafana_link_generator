fb9fc0029c1f188fd3f61f9f31f0d5d536c02fe2
Перов Алексей <p-er@yandex.ru>
2025-01-24 12:25:03 +0300

    start

--- background.js
+++ background.js
@@ -1,40 +1,39 @@
 // Добавляем слушатель событий для завершенных HTTP-запросов
 chrome.webRequest.onCompleted.addListener(
-	function(details) {
+	function (details) {
 		// Получаем значения из локального хранилища браузера
-		chrome.storage.local.get(['headerName', 'grafanaDomain', 'template'], function(items) {
+		chrome.storage.local.get([ 'headerName', 'grafanaDomain', 'template', 'datasource', 'orgId' ], function (items) {
 			// Используем значения по умолчанию, если они не установлены
-			const headerName = items.headerName || 'request-id';
-			const grafanaDomain = items.grafanaDomain || 'https://monitoring.dev.relef.dev';
-
-			const template = encodeURIComponent('{requestId="%s"} |= ``'); // Замените на ваш запрос
-			const orgId = 1; // ID организации в Grafana
+			const headerName = items.headerName;
+			const grafanaDomain = items.grafanaDomain;
+			const template = items.template;
+			const datasource = items.datasource;
+			const org = items.orgId;
 
 			// Ищем заголовок ответа с именем, указанным в headerName
 			const requestId = details.responseHeaders.find(header => header.name.toLowerCase() === headerName.toLowerCase());
 			if (requestId) {
-
-
+				const formattedTemplate = template.replace('%s', requestId.value);
 				// Функция для создания ссылки на Grafana Loki
-				function createLokiLink(grafanaUrl, orgId, query,datasource) {
+				function createLokiLink(grafanaUrl, orgId, datasource, query) {
 					const panes = {
 						"2xx": {
-							"datasource": "P8E80F9AEF21F6940",
+							"datasource": "" + datasource + "",
 							"queries": [
 								{
 									"refId": "A",
-									"expr": "{requestId=\""+requestId.value+"\"} |= ``",
+									"expr": "" + query + "",
 									"queryType": "range",
 									"datasource": {
 										"type": "loki",
-										"uid": "P8E80F9AEF21F6940"
+										"uid": "" + datasource + ""
 									},
 									"editorMode": "builder"
 								}
 							],
 							"range": {
-								"from": "now-1h",
-								"to": "now"
+								"from": "now-15m",
+								"to": "now+15m"
 							}
 						}
 					};
@@ -47,14 +46,17 @@ chrome.webRequest.onCompleted.addListener(
 						panes: JSON.stringify(panes)
 					});
 
-					const url = `${grafanaUrl}/explore?${params.toString()}`
+					const url = `${ grafanaUrl }/explore?${ params.toString() }`
 
-					return  url;
+					return url;
+
 				}
-				const grafanaLink = createLokiLink(grafanaDomain, orgId, template);
+
+				console.log(grafanaDomain, org, datasource, template);
+				const grafanaLink = createLokiLink(grafanaDomain, org, datasource, formattedTemplate);
 
 				// Получаем список всех сохраненных запросов из локального хранилища браузера
-				chrome.storage.local.get(['requests'], function(result) {
+				chrome.storage.local.get([ 'requests' ], function (result) {
 					// Добавляем новый запрос в список
 					let requests = result.requests || [];
 					requests.push({ url: details.url, requestId: requestId.value, grafanaLink });
@@ -64,16 +66,48 @@ chrome.webRequest.onCompleted.addListener(
 			}
 		});
 	},
-	{ urls: ["<all_urls>"] },
-	["responseHeaders"]
+	{ urls: [ "<all_urls>" ] },
+	[ "responseHeaders" ]
 );
 
-// Добавляем слушатель событий для установки расширения
-chrome.runtime.onInstalled.addListener(() => {
-	// Устанавливаем значения по умолчанию для headerName, grafanaDomain и template в локальном хранилище браузера
+
+chrome.runtime.onInstalled.addListener((details) => {
+	const currentVersion = chrome.runtime.getManifest().version;
+
+	if (details.reason === 'install') {
+		// При первоначальной установке расширения
+		initializeDefaultSettings(currentVersion);
+	} else if (details.reason === 'update') {
+		// При обновлении расширения
+		chrome.storage.local.get(['version'], (result) => {
+			const previousVersion = result.version;
+			if (previousVersion !== currentVersion) {
+				// Версия изменилась, выполним обновление настроек
+				updateSettings(currentVersion);
+			}
+		});
+	}
+});
+
+function initializeDefaultSettings(version) {
 	chrome.storage.local.set({
 		headerName: 'request-id',
-		grafanaDomain: 'https://monitoring.dev.relef.dev',
-		template: '{requestId="%s"} |= ``'
+		grafanaDomain: 'https://grafana.example',
+		template: '{requestId="%s"} |= ``',
+		datasource: ' ',
+		orgId: '1',
+		version: version
+	}, () => {
+		console.log('Настройки установлены по умолчанию.');
 	});
-});
+}
+
+function updateSettings(version) {
+	// Здесь можно добавить логику для обновления настроек при необходимости
+	// Например, добавить новые параметры или изменить существующие
+
+	// Обновим версию в хранилище
+	chrome.storage.local.set({ version: version }, () => {
+		console.log(`Расширение обновлено до версии ${version}.`);
+	});
+}
---
+++ options.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// Загружаем текущие настройки из локального хранилища
+	chrome.storage.local.get(['headerName', 'grafanaDomain', 'template', 'datasource', 'orgId'], function(items) {
+		document.getElementById('headerName').value = items.headerName ;
+		document.getElementById('grafanaDomain').value = items.grafanaDomain ;
+		document.getElementById('template').value = items.template ;
+		document.getElementById('datasource').value = items.datasource;
+		document.getElementById('orgId').value = items.orgId ;
+
+		console.log(items);
+	});
+
+	// Обработчик события сохранения настроек
+	document.getElementById('saveButton').addEventListener('click', function() {
+		const headerName = document.getElementById('headerName').value;
+		const grafanaDomain = document.getElementById('grafanaDomain').value;
+		const template = document.getElementById('template').value;
+		const datasource = document.getElementById('datasource').value;
+		const orgId = document.getElementById('orgId').value;
+
+		chrome.storage.local.set({
+			headerName: headerName,
+			grafanaDomain: grafanaDomain,
+			datasource: datasource,
+			orgId: orgId,
+			template: template
+		}, function() {
+			alert('Настройки сохранены.');
+		});
+	});
+});
